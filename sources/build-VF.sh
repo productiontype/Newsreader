b84b0d07276939f30872976e0f17bef09fe2f7b0
Yanone <post@yanone.de>
2020-07-02 13:13:22 +0200

    Mildly restructured build scripts

--- sources/build-VF.sh
+++ sources/build-VF.sh
@@ -1,11 +1,15 @@
 # Build variable fonts
 
-path="../fonts/variable/ttf"
+fontpath="../fonts/variable/ttf"
 
-mkdir -p $path
+mkdir -p $fontpath
+rm -r $fontpath/*.ttf
 
-fontmake -m NewsreaderVF-upright.designspace -o variable --output-path "$path/Newsreader[opsz,wght].ttf"
-fontmake -m NewsreaderVF-italics.designspace -o variable --output-path "$path/Newsreader-Italic[opsz,wght].ttf"
+# Fail fast
+set -e
+
+fontmake -m NewsreaderVF-upright.designspace -o variable --output-path "$fontpath/Newsreader[opsz,wght].ttf"
+fontmake -m NewsreaderVF-italics.designspace -o variable --output-path "$fontpath/Newsreader-Italic[opsz,wght].ttf"
 
 echo "Post generate vf"
 echo $PWD
--- sources/build-static.sh
+++ sources/build-static.sh
@@ -9,9 +9,12 @@
 # - gftools: https://github.com/googlefonts/gftools
 # - sfnt2woff: https://github.com/kseo/sfnt2woff
 # - woff2_compress: https://github.com/google/woff2
-# - ufoNormalizer: https://github.com/unified-font-object/ufoNormalizer
 # ----------------------------------------------------------------- #
 
+rm -r ../fonts/static/ttf/*.ttf
+
+# Fail fast
+set -e
 
 echo "Building static TTFs"
 fontmake -m NewsreaderStatic-upright.designspace -o ttf -i -a --output-dir ../fonts/static/ttf
@@ -26,3 +29,7 @@ sh ./tool-static/post_generate_static-ttf.sh
 # Build web fonts
 echo "Export webfonts"
 sh ./tool-static/export-webfont.sh
+
+
+# Delete instances, we don't need them. If you need to look at instances, comment-out this line and repeat
+rm -r instances
